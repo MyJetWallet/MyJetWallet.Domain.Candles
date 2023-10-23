3bde09e3b68847abfe7bf9a1ce3e5bdd9e2c65fd
Alexey Novichikhin <alexey.n@smplt.net>
2023-10-23 22:10:08 +0300

    update bidask

--- src/MyJetWallet.Domain.Candles/BidAsk.cs
+++ src/MyJetWallet.Domain.Candles/BidAsk.cs
@@ -28,14 +28,14 @@ namespace MyJetWallet.Domain.Candles
         [DataMember(Order = 4)]
         public double Ask { get; set; }
 
-        [DataMember(Order = 5)]
-        public long UnixTimeSec
+        [DataMember(Order = 6)]
+        public long UnixTime
         {
             get => _unixTimeSec;
             set => _unixTimeSec = value;
         }
         
-        [DataMember(Order = 6)]
+        [DataMember(Order = 7)]
         public double Last { get; set; }
 
         public static DateTime TimeFromLong(long ts) => ts.UnixTimeToDateTime();
