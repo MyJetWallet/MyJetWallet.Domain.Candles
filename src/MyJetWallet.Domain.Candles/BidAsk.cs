21f870fe7631744246c37c568c2ecf629f6de95c
Alexey Novichikhin <alexey.n@smplt.net>
2023-10-23 22:17:46 +0300

    update

--- src/MyJetWallet.Domain.Candles/BidAsk.cs
+++ src/MyJetWallet.Domain.Candles/BidAsk.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Runtime.Serialization;
-using DotNetCoreDecorators;
 
 namespace MyJetWallet.Domain.Candles
 {
@@ -8,37 +7,21 @@ namespace MyJetWallet.Domain.Candles
     public class BidAsk
     {
         public const string TopicName = "spot-bidask";
-        
-        private long _unixTimeSec;
 
         [DataMember(Order = 1)]
         public string Id { get; set; }
-
-        [Obsolete]
+        
         [DataMember(Order = 2)]
-        public DateTime DateTime
-        {
-            get => TimeFromLong(_unixTimeSec);
-            set => _unixTimeSec = TimeToLong(value);
-        }
+        public DateTime DateTime { get; set; }
 
         [DataMember(Order = 3)]
         public double Bid { get; set; }
 
         [DataMember(Order = 4)]
         public double Ask { get; set; }
-
-        [DataMember(Order = 6)]
-        public long UnixTime
-        {
-            get => _unixTimeSec;
-            set => _unixTimeSec = value;
-        }
         
-        [DataMember(Order = 7)]
-        public double Last { get; set; }
+        [DataMember(Order = 6)]
+        public decimal Last { get; set; }
 
-        public static DateTime TimeFromLong(long ts) => ts.UnixTimeToDateTime();
-        public static long TimeToLong(DateTime ts) => ts.UnixTime();
     }
 }
